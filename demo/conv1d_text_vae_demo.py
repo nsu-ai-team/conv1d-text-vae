36e7c35a03c0b6daf4c4ca80b5e0b5ed21901efe
Ivan Bondarenko <bond005@yandex.ru>
2018-09-18 17:10:41 +0700

    Уулчшен процесс обучения VAE (слишком большой набор векторов слов для выходного сигнала кластеризуется до желаемого размера).

--- demo/conv1d_text_vae_demo.py
+++ demo/conv1d_text_vae_demo.py
@@ -300,7 +300,7 @@ def main():
         en_fasttext_model = load_english_fasttext()
         vae = Conv1dTextVAE(input_embeddings=en_fasttext_model, output_embeddings=ru_fasttext_model,
                             n_filters=(512, 1024), kernel_size=3, latent_dim=500, n_recurrent_units=512,
-                            max_epochs=max_epochs, verbose=verbose, batch_size=minibatch_size)
+                            max_epochs=max_epochs, verbose=verbose, batch_size=minibatch_size, output_onehot_size=50000)
         vae.fit(input_texts_for_training, target_texts_for_training)
         print('')
         print('Training has been successfully finished.')
